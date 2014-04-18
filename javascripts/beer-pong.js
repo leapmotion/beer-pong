9c8be52270d94a1b0d7da11e5aa8655631d07d47
Raimo Tuisku <raimo@leapmotion.com>
2014-04-17 19:27:20 -0700

    Attempting to add JSON frames to firebase

--- javascripts/beer-pong.js
+++ javascripts/beer-pong.js
@@ -3,6 +3,8 @@ window.TO_DEG = 1 / TO_RAD;
 
 var palmPositionHud = document.getElementById('palmPosition');
 var palmVelocityHud = document.getElementById('palmVelocity');
+var gameId = window.location.hash.split('#')[1];
+window.playbackPlayer = null;
 
 (window.controller = new Leap.Controller)
   .use('riggedHand', {
@@ -25,27 +27,39 @@ var palmVelocityHud = document.getElementById('palmVelocity');
 
     // todo: pinch events
     if (hand.pinchStrength > 0.5) {
+      if (!window.recordFrames && gameId) {
+        console.log('start recording');
+        window.recordFrames = firebase.child('games').child(gameId).child('turns');
+      }
+      if (window.playbackPlayer && window.recordFrames) {
+        console.log(playbackPlayer.currentFrame());//.currentFrame()
+        window.recordFrames.push(playbackPlayer.currentFrame());
+      }
+
       // may need to use constraints for this
       pongBall.inHand = true;
       pongBall.position.copy(mesh.position);
       pongBall.physicsObject.velocity.set.apply(pongBall.physicsObject.velocity, hand.palmVelocity.map(function(num){ return num / 2000}))
-    }else{
+    } else {
+      console.log('stop recording');
+      window.recordFrames = false;
       pongBall.inHand = false;
     }
   });
+  window.playbackPlayer = controller.use('playback').plugins.playback.player; // we might want to reconsider how people use this - Raimo
 
 Game.begin();
 
-var gameId = window.location.hash.split('#')[1];
 if (gameId) {
-  firebase.child('/games/' + gameId + '/turns').on('child_added', function (turn) {
-    Game.overlay(turn.val().name + ' throws!');
-    controller.use('playback', {
-      autoPlay: true,
-      recording: 'perfection.lz',
-      pauseOnHand: false,
-      loop: true
-    });
+  firebase.child('/games/' + gameId + '/turns').on('child_added', function (gameTurns) {
+    return;
+    window.recordFrames = false;
+    window.remoteFrames = true;
+    Game.overlay(gameTurns.val().name + ' throws!');
+    gameTurns.on('child_added', function (turn) {
+      playbackPlayer.sendFrame(turn.val());
+      playbackPlayer.advanceFrame();
+    })
   });
 }
 
