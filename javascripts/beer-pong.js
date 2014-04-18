637bc0527c2ba63e912df7e2ecf76c079d72682e
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2014-04-17 20:31:16 -0700

    Move debugger gui to own js

--- javascripts/beer-pong.js
+++ javascripts/beer-pong.js
@@ -59,24 +59,3 @@ if (gameId) {
     });
   });
 }
-
-
-window.gui = new dat.GUI({
-  autoPlace: false
-});
-
-x = {gravity: 1}
-var baseGravity = world.gravity.y;
-
-gui.add(x, 'gravity',0, 10).onChange(function(value) {
-  // Fires on every change, drag, keypress, etc.
-  world.gravity.y = baseGravity * value;
-});
-
-document.getElementById('hud').appendChild(gui.domElement);
-
-$(function() {
-    if (/debug/.test(window.location.href)) {
-        $('.debug').css('visibility', 'visible');
-    }
-});
